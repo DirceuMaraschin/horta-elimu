505e0388027e11e346cbbf4402fc88660f53972e
Dirceu Maraschin Jr <dirceu_maraschin@hotmail.com>
2019-08-29 13:14:31 -0300

    simples adjust

--- irrigation-project.ino
+++ irrigation-project.ino
@@ -9,7 +9,7 @@
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-// variáveis
+// declaracao das variaveis
 const int pinoSensor = A0;
 const int pinoValvula = 10;
 
@@ -17,8 +17,8 @@ const int pinBuzzer = 9;
 const int pinAgua = A2;
 float water = 0.0;
 
-const float limiarSeco = 50.0; // Percentual EMPÍRICO de umidade
-const int tempoRega = 5; // Tempo de rega em segundos
+const float limiarSeco = 50.0; // Percentual empirico de umidade
+const int tempoRega = 12; // Tempo de rega em segundos
 float umidadeSolo = 0.0;
 float umidadeReal = 0.0;
 float convert = 0.0;
@@ -39,7 +39,7 @@ void setup() {
 
 void loop() {
   // Mede a umidade a cada x segundos. Uma hora = 3600 segundos.
-  for(int i=1; i<=10; i++) {
+  for(int i=1; i<=600; i++) {
     // Posiciona o cursor do LCD na coluna 0 linha 1 (Obs: linha 0 é a primeira)
     lcd.setCursor(0, 1);
     // Faz a leitura do sensor de umidade do solo
@@ -55,18 +55,16 @@ void loop() {
 
   // Conversão para único valor
   // divisão pelo mesmo valor de iter do laço 
-  umidadeReal = umidadeReal/10.0;
+  umidadeReal = umidadeReal/600.0;
 
   // verificação se o reservatório possui água
-  // se sim, a saída é zero, senão, valor
   water = analogRead(pinAgua);
-  if(water > 0.0){
+  if(water > 0.00){
     lcd.setCursor(0, 1);
     // Exibe a mensagem no Display LCD:
     lcd.print("  > SEM AGUA! <  ");
-    
-    // Aciona o buzzer na frequencia relativa ao 'Dó' em Hz
-    for(int i=0; i<10; i++){
+    // Aciona o buzzer na frequencia relativa ao 'Dó' em Hz 5 vezes
+    for(int i=0; i<5; i++){
       tone(pinBuzzer,261);
       delay(500);
       //Desativa o buzzer
@@ -78,11 +76,13 @@ void loop() {
   }
   
   else if(umidadeReal < limiarSeco) {
-    // Posiciona o cursor do LCD na coluna 0 linha 1
-    // (Obs: linha 1 é a segunda linha, a contagem começa em 0
     lcd.setCursor(0, 1);
     // Exibe a mensagem no Display LCD:
     lcd.print("    Regando!    ");
+    // Aciona o buzzer na frequencia relativa ao 'Mi' em Hz
+    tone(pinBuzzer,329);      
+    delay(500);
+    noTone(pinBuzzer);
     // Liga Bomba
     digitalWrite(pinoValvula, HIGH);
     // Mantém bomba ligada por tempo determinado
